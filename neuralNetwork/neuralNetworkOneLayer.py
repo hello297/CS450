60430b4bf37bf09771fa1301fe4141683e059a2a
Takeshi Yanagita <Takeshi Yanagita>
2016-10-15 18:34:06 -0600

    got the pima diabetes working

--- neuralNetwork/neuralNetworkOneLayer.py
+++ neuralNetwork/neuralNetworkOneLayer.py
@@ -12,6 +12,7 @@ class neuralNetworkOneLayer:
     def __init__(self, targets):
         results = []
         self.targetCount = len(set(targets))
+        print(self.targetCount)
 
     def train(self, inputs, targets):
         self.Nodes = []
@@ -31,7 +32,7 @@ class neuralNetworkOneLayer:
             else:
                 total = 0
             temp.append(total)
-            if inc % 3 == 2:
+            if inc % self.targetCount == self.targetCount - 1:
                 output.append(temp)
                 temp = []
         return output
@@ -55,19 +56,9 @@ irisNeural = neuralNetworkOneLayer(iris.target)
 irisNeural.train(iris.data, iris.target)
 print(irisNeural.predict())
 
-
-
-file = open("C:/Users/Takeshi Yangita/Desktop/2016 Fall/CS 450/neuralNetwork/pimaDiabetes.csv", "r")
-reader = csv.reader(file, skipinitialspace=False)
-pimaData = []
-for train in reader:
-    pimaData.append(np.array([float(i) for i in train]))
-
-file = open("C:/Users/Takeshi Yangita/Desktop/2016 Fall/CS 450/neuralNetwork/pimaDiabetesTarget.csv", "r")
-reader = csv.reader(file, skipinitialspace=False)
-pimaTargets = []
-for train in reader:
-    pimaTargets.append(np.array([int(i) for i in train]))
+pimaData = np.array(list(csv.reader(open("C:/Users/Takeshi Yangita/Desktop/2016 Fall/CS 450/neuralNetwork/pimaDiabetes.csv","r"),delimiter=','))).astype('float')
+pimaTargets = np.array(list(csv.reader(open("C:/Users/Takeshi Yangita/Desktop/2016 Fall/CS 450/neuralNetwork/pimaDiabetesTarget.csv","r"),delimiter=','))).astype('int')
+pimaTargets = pimaTargets.flatten()
 
 pimaNeural = neuralNetworkOneLayer(pimaTargets)
 pimaNeural.train(pimaData, pimaTargets)
